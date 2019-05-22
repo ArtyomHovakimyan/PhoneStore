8c69be3a606b589dc15afa44149dff8adca5127d
User <artyom.hovakimyan01@gmail.com>
2019-05-23 01:45:39 +0400

    add data folder and models

---
+++ PhoneStore/Data/Models/Phone.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PhoneStore.Data.Models
+{
+    public class Phone
+    {
+        public int PhoneId { get; set; }
+        public string Description { get; set; }
+        public string FullDescription { get; set; }
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; }
+        public string ImagesUrl { get; set; }
+        public bool IsPrefPhone { get; set; }
+        public int InS { get; set; }
+        public int CompanyId { get; set; }
+        public virtual Compnay Compnay { get; set; }
+    }
+}
